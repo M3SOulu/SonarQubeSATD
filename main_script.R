74b0351fc97ce6f45764a4c47796aea0a9392733
Leevi Rantala <leevi.rantala@oulu.fi>
2021-11-02 10:09:26 +0200

    Added rest of the file analyses
    The rest of the file analyses are added. Possibility to replicate
    the paper is now possible.

--- main_script.R
+++ main_script.R
@@ -19,7 +19,9 @@ load_and_process_data_file <- function(level="commit", satd_type="add", data_fil
   }
   else if(level=="file")
   {
-    
+    # Removing the issues of TODO and FIXME
+    df_temp <- df_temp[-grep("squid\\.S1134", names(df_temp))]
+    df_temp <- df_temp[-grep("squid\\.S1135", names(df_temp))]
   }
   else
   {
@@ -75,9 +77,10 @@ load_sonar_measures <- function(data_filename = ""){
   return(sonar_measures_all)
 }
 
-# Normalize the data within projects
+# Normalize commit data within projects
 normalize_data_within_projects <- function(data_df = df)
 {
+  data_df[is.na(data_df)] <- 0
   data_df$ncloc_norm <- ave(data_df$ncloc, data_df$projectID, FUN=function(x) (x-min(x))/(max(x) - min(x)))
   data_df$sqaleIndex_norm <- ave(data_df$sqaleIndex, data_df$projectID, FUN=function(x) (x-min(x))/(max(x) - min(x)))
   data_df$reliabilityRemediationEffort_norm <- ave(data_df$reliabilityRemediationEffort, data_df$projectID, FUN=function(x) (x-min(x))/(max(x) - min(x)))
@@ -111,11 +114,50 @@ create_tables_1_and_2 <- function(data_df = df, level="commit")
   print(paste0("Normality test: ", shapiro_test$method, " , W = ", shapiro_test$statistic, " , p-value = ", shapiro_test$p.value))
 }
 
+# Finding redundant metrics with redun
 find_redundant_metrics <- function(data_df = df)
 {
   column_formula2 <- as.formula("~ ncloc_norm + sqaleIndex_norm + reliabilityRemediationEffort_norm + securityRemediationEffort_norm")
   print(redun(column_formula2, data=data_df))
 }
+
+# Creating file-level issue forumula
+create_issue_formula <- function(data_df = df)
+{
+  list_of_columns <- colnames(df)
+  list_of_columns <- list_of_columns[list_of_columns %like% "_norm$"]
+  # Removing TODO and FIXME issues
+  list_of_columns_no_fixme_todo <- list_of_columns[!list_of_columns %in% c("squid.S1134_norm", "squid.S1135_norm")]
+  column_formula <- paste(list_of_columns_no_fixme_todo, collapse = " + ")
+  column_formula <- paste("SATD ~ ", column_formula, sep = "")
+  column_formula <- paste(column_formula, " + (1 | projectID / fileName / id)", sep = "")
+  column_formula <- as.formula(column_formula)
+  return(column_formula)
+}
+
+# Normalize issue data within projects and files
+normalize_data_within_files_and_projects <- function(data_df = df)
+{
+  ### Normalize all the issues
+  odd_indexes<-seq(1,nrow(data_df),2)
+  data_df <- arrange(data_df, id, desc(SATD))
+  data_df[is.na(data_df)] <- 0
+  data_df <- data_df[, colSums(df != 0) > 0]
+  project_and_file_column <- c("projectID", "fileName")
+  
+  list_of_columns <- colnames(data_df)
+  list_of_columns <- list_of_columns[list_of_columns %like% "^code_smells" | list_of_columns %like% "^squid" | list_of_columns %like% "^common.java"]
+  for (column_name in list_of_columns){
+    data_df[,paste0(column_name, "_norm")] <- NA
+    data_df[,paste0(column_name, "_norm")] <- ave(data_df[,column_name], data_df[,project_and_file_column], FUN=function(x) (x-min(x))/(max(x) - min(x)))
+  }
+  # Replace possible NaNs
+  is.nan.data.frame <- function(x) do.call(cbind, lapply(x, is.nan))
+  data_df[is.nan(data_df)] <- 0
+  return(data_df)
+}
+
+
 ##### End of functions #####
 
 ### Start of commit-level analyses
@@ -179,102 +221,18 @@ df <- left_join(df, sonar_measures, by = c("commitHash", "projectID"))
 # 4.3. Eliminate redundant / incomplete data
 df <- eliminate_redundant_data(data_df = df)
 
+# 4.4. Normalize data within projects
+df <- normalize_data_within_files_and_projects(data_df = df)
+
 # 5.0 Making the Table 2 for the paper
 # Table 2 addition row is made with file-level added data, and deletion row with file-level deleted data
 create_tables_1_and_2(data_df = df, level = "file")
 
-# 5.1 Finding the possibly redundant commit-level metrics
-# p.12 "Before running the analysis on commit-level with Sqale Index, and the 2 Remediation Efforts, 
-# we wanted to avoid multi-collinearity issues with different metrics. This was done by utilizing redun-function
-# from Hmisc-package in R. We used the default threshold for cutoff (R\textsuperscript{2} 0.9).
-find_redundant_metrics(data_df = df)
+# 6.0 Creating the file-level formula
+issue_formula <- create_issue_formula(data_df = df)
 
-
-
-
-### Here we load the data. This is already interleaved data containing all of Sonar Issues
-
-# File-level
-df_a <- read.csv(file = "./data/comments/todo_fixme_file_data_added_june_21.csv", fileEncoding = "utf-8", stringsAsFactors = FALSE)
-df_d <- read.csv(file = "./data/comments/todo_fixme_file_data_deleted_june_21.csv", fileEncoding = "utf-8", stringsAsFactors = FALSE)
-
-# Choose here the df, you want to use
-df <- df_a
-df <- df_d
-
-########## Combine with Sonarqube measures
-sonar_measures_all <- read.csv(file = "data/tdd_10/sonar_measures_all.csv", stringsAsFactors = FALSE, strip.white = TRUE)
-sonar_measures_all$commitHash <- str_trim(sonar_measures_all$commitHash)
-sonar_measures_all$projectID <- str_trim(sonar_measures_all$projectID)
-# ncloc, reliability, security
-sonar_measures_all <- sonar_measures_all[,c(1:3,34, 48, 56, 58)]
-
-# Depending on, whether you analyze additions or deletions, choose the right join operation
-df <- left_join(df, sonar_measures_all, by = c("commit_added" = "commitHash", "projectID"))
-df <- left_join(df, sonar_measures_all, by = c("commit_deleted" = "commitHash", "projectID"))
-
-# Eliminating the pairs, where at least one didn't have an analysis date
-df <- df[!is.na(df$SQAnalysisDate),]
-na_ids <- df$id
-na_ids <- na_ids[duplicated(na_ids)]
-df <- subset(df, (id %in% na_ids))
-
-# Eliminating the pairs, where commit level metrics were all zero
-df <- df[df$ncloc != 0 & df$sqaleIndex != 0 & df$reliabilityRemediationEffort != 0 & df$securityRemediationEffort != 0,]
-na_ids <- df$id
-na_ids <- na_ids[duplicated(na_ids)]
-df <- subset(df, (id %in% na_ids))
-
-## Now for the file level
-df_agg <- aggregate(df$SATD, df[,c("projectID", "commit_added", "file_added")], FUN=length)
-df_agg <- aggregate(df$SATD, df[,c("projectID", "commit_deleted", "file_deleted")], FUN=length)
-
-### Table 1 for paper
-# Number of pairs
-nrow(df)/2
-#KL-SATD in Commits summaries
-summary(df_agg$x)
-# Normality test
-shapiro.test(df_agg$x)
-
-# We remove the SQUID.1135 and SQUID.1134 columns, which are SonarQube's
-# issues for TODO and FIXME comments. So they might have huge coefficients.
-df <- df[-grep("squid\\.S1134", names(df))]
-df <- df[-grep("squid\\.S1135", names(df))]
-
-### Normalize all the issues
-odd_indexes<-seq(1,nrow(df),2)
-df <- arrange(df, id, desc(SATD))
-df[is.na(df)] <- 0
-df <- df[, colSums(df != 0) > 0]
-# Choose the right depending on which df you are using
-project_and_file_column <- c("projectID", "file_added")
-project_and_file_column <- c("projectID", "file_deleted")
-
-list_of_columns <- colnames(df)
-list_of_columns <- list_of_columns[list_of_columns %like% "^code_smells" | list_of_columns %like% "^squid" | list_of_columns %like% "^common.java"]
-for (column_name in list_of_columns){
-  df[,paste0(column_name, "_norm")] <- NA
-  df[,paste0(column_name, "_norm")] <- ave(df[,column_name], df[,project_and_file_column], FUN=function(x) (x-min(x))/(max(x) - min(x)))
-}
-
-# Replace possible NaNs
-is.nan.data.frame <- function(x) do.call(cbind, lapply(x, is.nan))
-df[is.nan(df)] <- 0
-
-# Creating the Issue formulas
-list_of_columns <- colnames(df)
-list_of_columns <- list_of_columns[list_of_columns %like% "_norm$"]
-list_of_columns_no_fixme_todo <- list_of_columns[!list_of_columns %in% c("squid.S1134_norm", "squid.S1135_norm")]
-column_formula <- paste(list_of_columns_no_fixme_todo, collapse = " + ")
-
-column_formula <- paste("SATD ~ ", column_formula, sep = "")
-column_formula <- paste(column_formula, " + (1|projectID/file_added/id)", sep = "")
-column_formula <- paste(column_formula, " + (1|projectID/file_deleted/id)", sep = "")
-
-column_formula <- as.formula(column_formula)
-
-# Running the issue analysis
-# Tables 9 & 11
-lmer_model_no_todo_fixme_norm <- glmer(column_formula, data = df, family = "binomial")
-summary(lmer_model_no_todo_fixme_norm)
+# 6.0  Running the file-level analyses
+# This is the main part, running this with file-level additions makes the Table 9.
+# Running this with commit-level deletions makes the Table 11.
+lmer_model <- glmer(issue_formula, data = df, family = "binomial")
+summary(lmer_model)
